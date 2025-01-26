494ab3c5de4d3b13e066eaceaf59f28bf16caf4d
Elia <98826251+Elia1802@users.noreply.github.com>
2025-01-26 17:42:03 +0100

    update version of library

--- src/main/java/de/elia/PluginInfo.java
+++ src/main/java/de/elia/PluginInfo.java
@@ -12,7 +12,7 @@ public class PluginInfo {
 
   public static class SoulLibrary {
     public static final String NAME = "SoulLibrary";
-    public static final String VERSION = "5.0.0-dev";
+    public static final String VERSION = "5.0.0";
   }
 
 }
